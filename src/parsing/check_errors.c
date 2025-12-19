56809f7391a3f0bfa7e38b237c0204a3b382299a
Yasir Bel Maalem Ouhadou Abdenour <ybel-maa@c2r4s3.42malaga.com>
2025-12-19 15:37:02 +0100

    parsing

--- src/parsing/check_errors.c
+++ src/parsing/check_errors.c
@@ -3,58 +3,75 @@
 /*                                                        :::      ::::::::   */
 /*   check_errors.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ybel-maa <ybel-maa@student.42malaga.com>   +#+  +:+       +#+        */
+/*   By: ybel-maa <ybel-maa@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/17 11:55:13 by ybel-maa          #+#    #+#             */
-/*   Updated: 2025/12/17 11:55:13 by ybel-maa         ###   ########.fr       */
+/*   Updated: 2025/12/19 15:06:14 by ybel-maa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int is_number(char *s)
+int	is_number(char *s)
 {
-    int i;
-    i = 0;
-    if(!s[i])
-        return (0);
-    if (s[i] == '-' || s[i] == '+')
-        i++;
-    while (s[i])
-    {
-        if (s[i] < '0' || s[i] > '9')
-            return (0);
-        i++;
-    }
-    return (1);
+	int	i;
+
+	i = 0;
+	if (!s[i])
+		return (0);
+	if (s[i] == '-' || s[i] == '+')
+		i++;
+	while (s[i])
+	{
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+		i++;
+	}
+	return (1);
 }
 
-int is_overflow(char *s)
+int	is_overflow(char *s)
 {
-    long num;
-    int sign;
-    int i;
+	long	num;
+	int		sign;
+	int		i;
 
-    num = 0;
-    sign = 1;
-    i = 0;
-    if (s[i] == '-' || s[i] == '+')
-    {
-        if (s[i] == '-')
-            sign = -1;
-        i++;
-    }
-    while (s[i])
-    {
-        num = num * 10 + (s[i] - '0');
-        if((sign == 1 && num > 2147483647) || (sign == -1 && -num < -2147483648))
-            return (0);
-        i++;
-    }
-    return (1);
+	num = 0;
+	sign = 1;
+	i = 0;
+	if (s[i] == '-' || s[i] == '+')
+	{
+		if (s[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (s[i])
+	{
+		num = num * 10 + (s[i] - '0');
+		if ((sign == 1 && num > 2147483647) || (sign == -1 && -num <
+				-2147483648))
+			return (0);
+		i++;
+	}
+	return (1);
 }
 
-int check_duplicates(t_stack *a);
+int	check_duplicates(t_stack *a)
 {
-    
+	t_node	*i;
+	t_node	*j;
+
+	i = a->top;
+	while (i)
+	{
+		j = i->next;
+		while (j)
+		{
+			if (i->value == j->value)
+				return (0);
+			j = j->next;
+		}
+		i = i->next;
+	}
+	return (1);
 }
--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parsing.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ybel-maa <ybel-maa@student.42malaga.com>   +#+  +:+       +#+        */
+/*   By: ybel-maa <ybel-maa@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/17 11:55:13 by ybel-maa          #+#    #+#             */
-/*   Updated: 2025/12/17 11:55:13 by ybel-maa         ###   ########.fr       */
+/*   Updated: 2025/12/19 13:59:58 by ybel-maa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,7 @@ void parse_multiple_args(int argc, char **argv, t_stack *a)
 
     while (i > 0)
     {
-        if(!is_number(argv[i]) || !is_overflow(argv[i]))
+        if(!is_number(argv[i]) || !is_overflow(argv[i]) || !check_duplicates(argv[i]))
         {
             free_stack(a);
             print_error();
